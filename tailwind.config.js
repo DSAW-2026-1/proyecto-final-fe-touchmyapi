5508b6355aa9ef859642a96d489ea4855ffe0e96
Jusselth <jusselthchro@unisabana.edu.co>
2026-05-06 20:55:28 -0500

    LoginPage with corrections

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,18 @@ export default {
     ],
     theme: {
       extend: {
+        colors: {
+          'sabana-blue': '#001C64', // Azul oscuro Sabana para fondo y botones
+          'sabana-light': '#E0EDFF', // Azul claro Sabana para fondo
+          'sabana-blue-light': '#9DC4FF', // Azul claro Sabana para texto #90CAF9
+          'sabana-softGold': '#FFFADA', // Soft Gold Sabana color piel para errores
+          'error-red': '#B00020', // Rojo error
+          'error-bg-red': '#FFEBEE', // Fondo rojo error
+          'default-white': '#FFFFFF', // Blanco por defecto
+          'default-gray': '#424242', // Gris por defecto
+          'defaultBorder-gray': '#EDF1F3', // Gris por defecto para bordes
+        },
+
         fontFamily: {
           'roboto': ['Roboto', 'sans-serif'],
           'roboto-slab': ['Roboto Slab', 'serif'],
